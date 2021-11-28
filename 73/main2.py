7a84cf4f624193c5211a7566462d06ab69c02073
Zhongren Shao <shao.zhongren@gmail.com>
2021-11-27 21:41:25 -0600

    did 2 more problems

---
+++ 73/main2.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        columns_to_set = set()
+        rows_to_set = set()
+        for i, row in enumerate(matrix):
+            for j, cell in enumerate(row):
+                if cell == 0:
+                    columns_to_set.add(j)
+                    rows_to_set.add(i)
+        for i in range(len(matrix)):
+            for j in len(matrix[i]):
+                if i in rows_to_set or j in columns_to_set:
+                    matrix[i][j] = 0
